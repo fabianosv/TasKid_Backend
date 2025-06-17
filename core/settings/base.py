b1b42d25861d80782e9733a68b78c7b913809906
riquelme.santos <riquelme.santos@fpf.br>
2025-06-17 17:59:05 -0400

    Push da base pro docker-compose

--- core/settings/base.py
+++ core/settings/base.py
@@ -63,11 +63,11 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'taskids_db',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 AUTH_PASSWORD_VALIDATORS = []
